5f773e6f3ddb58c0820739acc1e132f23330fb73
Serg <email@mail.com>
2025-02-25 14:17:19 -0500

    rest api done

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -164,9 +164,29 @@ class TestAccountService(TestCase):
         upd_account = resp.get_json()
         upd_account["name"] = "Ather Name"
         resp = self.client.put(
-            f"{BASE_URL}/{upd_account['id']}"
+            f"{BASE_URL}/{upd_account['id']}",
+            json=upd_account
         )
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         updated_account = resp.get_json()
         self.assertEqual(updated_account['name'], "Ather Name")     
     
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+
+        resp = self.client.delete(
+            f"{BASE_URL}/{account.id}"
+        )
+
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        
+        resp = self.client.delete(
+            BASE_URL
+        )
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
